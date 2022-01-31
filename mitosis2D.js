ce8bc36cd1617c7b891fdcd2e2202b4611011d8d
Hazel Kaine <94728019+hazelkaine@users.noreply.github.com>
2022-01-30 21:56:58 -0700

    Update mitosis2D.js

--- mitosis2D.js
+++ mitosis2D.js
@@ -1,16 +1,14 @@
 var cells =[];
 function setup() {
-    createCanvas(400, 400)
-    cells = new Cell();
-    cells.push(cell);
+    createCanvas(400, 400);
+    cells.push(new Cell());
  
 }
-  
+
   
   function draw() {
     background(51);
-   for (var i = 0; i<cells.length; i++){
-        cell[i].move();
-        cell[i].show();
-  }
+   for (var i = 0; i< cells.length; i++){
+        cells[i].move();
+        cells[i].show();
   }
